99deb1495e2da38cd487acc385c3e48deb309eae
Victor Marotta <victordossantosmarotta@gmail.com>
2025-04-04 14:23:39 -0300

    chore: add qgis as base image

--- build-docker.sh
+++ build-docker.sh
@@ -2,41 +2,34 @@
 
 IMAGE=$1
 
-if [ "$IMAGE" != "base" ] && [ "$IMAGE" != "runner" ] && [ "$IMAGE" != "gdal" ]; then
-  echo "ERROR: Please provide a valid image type - example: ./build-docker.sh base | ./build-docker.sh runner | ./build-docker.sh gdal"
+if [ "$IMAGE" != "base" ] && [ "$IMAGE" != "runner" ] && [ "$IMAGE" != "geo" ]; then
+  echo "ERROR: Please provide a valid image type - example: ./build-docker.sh base | ./build-docker.sh runner | ./build-docker.sh geo"
   exit 1
 fi
 
-if [ "$IMAGE" = "base" ]; then
+if [ "$IMAGE" = "base" ] || [ "$IMAGE" = "runner" ] || [ "$IMAGE" = "geo" ]; then
   echo "Building n8n base image"
   docker buildx build --load --platform linux/amd64 . \
     --file ./docker/images/n8n-base/Dockerfile \
     --tag paschendale/n8n-base:20-bookworm \
     --progress plain
   docker push paschendale/n8n-base:20-bookworm
-  exit 0
+  docker push paschendale/n8n-base:20-bookworm
 fi
 
 # Get the current version from package.json
 VERSION=$(grep '"version"' package.json | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[:space:]')
 
-echo "Building n8n runner image for version $VERSION"
+echo "\n#################################################################################################################################\n"
 
-if [ "$IMAGE" = "runner" ] || [ "$IMAGE" = "gdal" ]; then
-  echo "Building n8n runner image"
+if [ "$IMAGE" = "runner" ] || [ "$IMAGE" = "geo" ]; then
+  echo "Building n8n runner image for version $VERSION"  
   docker buildx build --load --platform linux/amd64 ./docker/images/n8n \
     --tag paschendale/n8n:$VERSION-bookworm \
     --tag paschendale/n8n:latest-bookworm \
+    --tag paschendale/n8n-geo:latest \
     --build-arg N8N_VERSION=$VERSION \
     --progress plain
   docker push paschendale/n8n:$VERSION-bookworm
-fi
-
-if [ "$IMAGE" = "gdal" ]; then
-  echo "Building n8n GDAL image"
-  docker buildx build --load --platform linux/amd64 \
-    ./docker/images/n8n-gdal \
-    --tag paschendale/n8n-gdal:latest \
-    --progress plain
-  docker push paschendale/n8n-gdal:latest
+  docker push paschendale/n8n:latest-bookworm  
 fi
